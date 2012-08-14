d8f5aa8e8e525aa9494c96d6b890c8516b430764
dzwitz <dzwitz@gmail.com>
2012-08-14 18:53:38 -0400

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -65,7 +65,7 @@ describe "Authentication" do
 	  end
         end
       end
-    
+         
       describe "in the Users controller" do
       
         describe "visiting the edit page" do
@@ -81,6 +81,19 @@ describe "Authentication" do
 	describe "visiting the user index" do
 	  before { visit users_path }
 	  it { should have_selector('title', text: 'Sign in') }
+	end
+      end
+      
+      describe "in the Microposts controller" do
+      
+        describe "submitting to the create action" do
+	  before { post microposts_path }
+	  specify { response.should redirect_to(signin_path) }
+	end
+	
+	describe "submitting to the destroy action" do
+	  before { delete micropost_path(FactoryGirl.create(:micropost)) }
+	  specify { response.should redirect_to(signin_path) }
 	end
       end
     end
